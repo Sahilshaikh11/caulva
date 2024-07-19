35943e1cb46bb90441fc9c4bd776c58c7269812f
Shahil Shaikh <shaikhsahil4495@gmail.com>
2024-07-20 00:22:17 +0530

    theme created

---
+++ lib/theme/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color accentColor = Color(0xFF004DEC);
+  static const Color backgroundColor = Color(0xFFF7F7FF);
+  static const Color cardColor = Color(0xFFFFFFFF);
+  static const Color barColor = Color(0xFFEDF0F7);
+  static const Color barFillColor = Color(0xFF16DBCC);
+  static const Color headingTextColor = Color(0xFF718EBF);
+  static const Color textColorBlack = Color(0xFF000000);
+  static const Color textColorWhite = Color(0xFFFFFFFF);
+  static const Color summaryCard1 = Colors.blue;
+  static const Color summaryCard2 = Colors.orange;
+  static const Color summaryCard3 = Colors.pink;
+  static const Color summaryCard4 = Colors.lightBlue;
+}
